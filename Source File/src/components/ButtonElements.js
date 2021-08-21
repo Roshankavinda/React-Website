328448e503c7b02c0a0943ab04cdf7bd557b4a10
Roshan Wickramasooriya <roshankavi1998@gmail.com>
2021-08-21 17:17:47 +0530

    updated

---
+++ Source File/src/components/ButtonElements.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import {Link} from 'react-scroll'
+
+export const Button = styled(Link)`
+   border-radius: 50px;
+   background: ${({primary}) => (primary ? '#6A5ACD' : '#6A5ACD')};
+   white-space: nowrap;
+   padding: ${({big}) => (big ? '14px 48px' : '12px 30px')};
+   color: ${({dark}) => (dark ? '#010606' : 'black')};
+   font-weight: bold;
+   font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')};
+   outline: none;
+   border-style: solid;
+   border-color: Black;
+   cursor: pointer;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   transition: all 0.2s ease-in-out;
+   font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+
+   &:hover{
+    transition: all 0.2s ease-in-out;
+    background: ${({primary}) => (primary ? '#fff' : '#36F57F')};
+   }
+`
